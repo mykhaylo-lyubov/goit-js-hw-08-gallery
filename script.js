07076a60c04b523a3c38aa76200fee65c45e1e6b
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2020-12-17 14:07:00 +0200

    module 8 task has been done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import primaryArrayOfImages from "./gallery-items.js";
 
 const refs = {
-  list: document.querySelector(".js-gallery"),
+  listOfImages: document.querySelector(".js-gallery"),
   divToOpenModal: document.querySelector(".js-lightbox"),
   btnToCloseModal: document.querySelector(
     "button[data-action='close-lightbox']"
@@ -12,7 +12,7 @@ const refs = {
 
 const createListItems = () => {
   let newArrOfTags = [];
-  primaryArrayOfImages.map((item) => {
+  primaryArrayOfImages.map((item, idx) => {
     const tagLi = document.createElement("li");
     tagLi.classList.add("gallery__item");
 
@@ -25,40 +25,98 @@ const createListItems = () => {
     imgTag.setAttribute("src", item.preview);
     imgTag.setAttribute("data-source", item.original);
     imgTag.setAttribute("alt", item.description);
+    imgTag.setAttribute("data-index", idx);
 
     tagA.append(imgTag);
     tagLi.append(tagA);
 
     newArrOfTags.push(tagLi);
   });
-  refs.list.append(...newArrOfTags);
+  refs.listOfImages.append(...newArrOfTags);
 };
-
 createListItems();
+let indexOfImgToShow;
 
-refs.list.addEventListener("click", (event) => {
+refs.listOfImages.addEventListener("click", onImgClickOpenModal);
+
+refs.btnToCloseModal.addEventListener("click", onBtnCloseModal);
+
+refs.divOverlay.addEventListener("click", onDivOverlay);
+
+function onImgClickOpenModal(event) {
+  window.addEventListener("keydown", onPressEscape);
+  window.addEventListener("keydown", onArrowKeys);
+
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
-
   const clickedImgRef = event.target.dataset.source;
+  const indexOfImgClicked = Number(event.target.dataset.index);
+  indexOfImgToShow = indexOfImgClicked;
   const clickedImgAltRef = event.target.getAttribute("alt");
 
   refs.divToOpenModal.classList.add("is-open");
 
   refs.modalContentImgRef.setAttribute("src", clickedImgRef);
   refs.modalContentImgRef.setAttribute("alt", clickedImgAltRef);
-});
+}
 
-refs.btnToCloseModal.addEventListener("click", () => {
+function onBtnCloseModal() {
+  window.removeEventListener("keydown", onPressEscape);
+  window.removeEventListener("keydown", onArrowKeys);
+  classAndAttributesRemoval();
+}
+
+function onPressEscape(event) {
+  if (event.code === "Escape") {
+    classAndAttributesRemoval();
+    onBtnCloseModal();
+  }
+}
+
+function onDivOverlay() {
+  window.removeEventListener("keydown", onPressEscape);
+  window.removeEventListener("keydown", onArrowKeys);
+  classAndAttributesRemoval();
+}
+
+function onArrowKeys(event) {
+  if (event.code === "ArrowLeft") {
+    indexOfImgToShow -= 1;
+    if (indexOfImgToShow < 0) {
+      indexOfImgToShow = 0;
+      return;
+    }
+    const nextImgToShowLeft = document.querySelector(
+      `img[data-index='${indexOfImgToShow}']`
+    );
+    const leftImgToShowRef = nextImgToShowLeft.dataset.source;
+    const leftImgAltRef = nextImgToShowLeft.getAttribute("alt");
+
+    refs.modalContentImgRef.setAttribute("src", leftImgToShowRef);
+    refs.modalContentImgRef.setAttribute("alt", leftImgAltRef);
+  }
+
+  if (event.code === "ArrowRight") {
+    indexOfImgToShow += 1;
+    if (indexOfImgToShow > primaryArrayOfImages.length - 1) {
+      indexOfImgToShow = primaryArrayOfImages.length - 1;
+      return;
+    }
+    const nextImgToShowRight = document.querySelector(
+      `img[data-index='${indexOfImgToShow}']`
+    );
+    const rightImgToShowRef = nextImgToShowRight.dataset.source;
+    const rightImgAltRef = nextImgToShowRight.getAttribute("alt");
+
+    refs.modalContentImgRef.setAttribute("src", rightImgToShowRef);
+    refs.modalContentImgRef.setAttribute("alt", rightImgAltRef);
+  }
+}
+
+function classAndAttributesRemoval() {
   refs.divToOpenModal.classList.remove("is-open");
   refs.modalContentImgRef.setAttribute("src", "");
   refs.modalContentImgRef.setAttribute("alt", "");
-});
-
-refs.divOverlay.addEventListener("click", () => {
-  refs.divToOpenModal.classList.remove("is-open");
-  refs.modalContentImgRef.setAttribute("src", "");
-  refs.modalContentImgRef.setAttribute("alt", "");
-});
+}
